6daf1832208e77e0869e39121234565eedeac698
IRONM00N <me@ironmoon.dev>
2025-08-21 10:35:00 -0400

    add nix pre-commit

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/25.05";
     systems.url = "github:nix-systems/default";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
@@ -9,14 +10,27 @@
       self,
       nixpkgs,
       systems,
+      pre-commit-hooks,
     }:
     let
       inherit (nixpkgs) lib legacyPackages;
       eachSystem = f: lib.genAttrs (import systems) (system: f legacyPackages.${system});
+      hooks = {
+        treefmt.enable = true;
+      };
     in
     {
+      checks = eachSystem (pkgs: {
+        pre-commit-check = pre-commit-hooks.lib.${pkgs.system}.run {
+          src = ./.;
+          inherit hooks;
+        };
+      });
+
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
+          inherit (self.checks.${pkgs.system}.pre-commit-check) shellHook;
+
           packages =
             [ ]
             ++ (with pkgs; [
