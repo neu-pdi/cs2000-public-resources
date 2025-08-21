ccb03ec5713b0eef52c52795856d60ed170f790b
IRONM00N <me@ironmoon.dev>
2025-08-21 10:12:07 -0400

    flake; setup formatters

--- src/plugins/variable-substitution.ts
+++ src/plugins/variable-substitution.ts
@@ -1,17 +1,18 @@
 import { visit } from 'unist-util-visit';
 
 export function createVariableSubstitutionPlugin(dcicDomain: string) {
+  const regex = /(?:\{\{|%7B%7B)DCIC_DOMAIN(?:\}\}|%7D%7D)/g;
   return function variableSubstitutionPlugin() {
     return (tree: any) => {
       visit(tree, 'text', (node) => {
         if (typeof node.value === 'string') {
-          node.value = node.value.replace(/\{\{DCIC_DOMAIN\}\}/g, dcicDomain);
+          node.value = node.value.replaceAll(regex.source, dcicDomain);
         }
       });
-      
+
       visit(tree, 'link', (node) => {
         if (typeof node.url === 'string') {
-          node.url = node.url.replace(/\{\{DCIC_DOMAIN\}\}/g, dcicDomain);
+          node.url = node.url.replaceAll(regex, dcicDomain);
         }
       });
     };
