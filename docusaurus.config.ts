22e3ab0d0f174018336c99a0b4173f519cc4aca1
loganscott74 <loganscottgill@gmail.com>
2025-10-08 19:02:14 -0400

    Updated NavBar To Open To This Weeks Items
    - The Homework, Recitation, and Lab nav bar buttons now open to this week's version
    - The Day tab now opens to the current/latest lecture day

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,6 +11,136 @@ import { oneDarkTheme, oneLightTheme } from './src/theme/one-dark-themes';
 // Configuration variables
 const dcicDomain = 'https://dcic.pdi.run';
 
+/**
+ * Calculates the current assignment number based on today's date. Assignments include recitations.
+ * After week 15, the assignment number resets to 1
+ *
+ * @returns The current assignment number, with a minimum of 1 and a max of 12
+ * 
+ * @author Logan Gill
+ */
+function currentAssignmentNumber(): string {
+  // Uses Thursday as a start date, since assignments are due Thursdays
+  const currWeek = currentWeekNumber(new Date(2025, 8, 4, 0, 0, 0));
+
+  // If the current week is greater than 15, reset to 1 since class is over
+  if (currWeek > 15) {
+    return '1';
+  }
+  // Return the current week number, up to 12, since there are only 12 assignments
+  return (currWeek > 12 ? 12 : currWeek).toString();
+}
+
+/**
+ * Calculates the current lab number based on today's date. After week 15, the lab number resets to 1
+ * 
+ * @returns The current lab number, with a minimum of 1 and a maximum of 10
+ * 
+ * @author Logan Gill
+ */
+function currentLabNumber(): string {
+  // Uses Friday as a start date, since the last lab of each week is hosted on Fridays
+  const currWeek = currentWeekNumber(new Date(2025, 8, 5, 0, 0, 0));
+
+  // If the current week is less than or equal to 8, return the current week as is
+  if (currWeek <= 8) {
+    return currWeek.toString();
+  } else if (currWeek <= 12) {
+    // If the current week is between 9 and 12, offset to account for 1st skill bundle
+    return (currWeek - 1).toString();
+  } else if (currWeek > 15) {
+    // If the current week is greater than 15, reset to 1 since class is over
+    return '1';
+  }
+  // If the current week is between 13 and 15, offset to account for both skill bundles, max at 10
+  return (currWeek - 2 > 10 ? 10 : currWeek - 2).toString();
+}
+
+/**
+ * Calculates the current lecture day number based on today's date. After week 15, shows the summery page
+ * 
+ * @returns Gets the current lecture day number to show
+ * 
+ * @author Logan Gill
+ */
+function currentDayNumber(): string {
+  // Uses Saturday as a start date, so that Monday is the start of the new week (since ``currentWeekNumber`` rounds up)
+  const currWeek = currentWeekNumber(new Date(2025, 7, 31, 0, 0, 0));
+
+  // If the current week is greater than 15, show the summary page
+  if (currWeek > 15) {
+    return 'l0-summary';
+  }
+
+  var currDay = 3 * currWeek;
+  const currDayOfWeek = new Date().getDay();
+  // Offset based on the current day of the week
+  if (currDayOfWeek >= 4 || currDayOfWeek === 0) {
+    // Offset by 1 for days after Thursday and when it is Sunday
+    currDay -= 1;
+  } else if (currDayOfWeek >= 3) {
+    // Offset by 2 for Wednesday
+    currDay -= 2;
+  } else {
+    // Offset by 3 for days before Wednesday
+    currDay -= 3;
+  }
+
+  // Offset to account for skill day 1 (on a Thursday)
+  if (currWeek > 5 || (currWeek === 5 && currDay >= 4)) {
+    currDay -= 1;
+  }
+
+  // Offset to account for holiday (on a Monday)
+  if (currWeek >= 7) {
+    currDay -= 1;
+  }
+
+  // Offset to account for skill day 2 (on a Thursday)
+  if (currWeek > 9 || (currWeek === 9 && currDay >= 4)) {
+    currDay -= 1;
+  }
+
+  // Offset to account for holiday
+  // Note: Unsure which day (Mon, Wed, Thu) is skipped since holiday is Tuesday
+  if (currWeek >= 11) {
+    currDay -= 1;
+  }
+
+  // Offset to account for holiday (Wednesday + Thursday)
+  if (currWeek > 13 || (currWeek === 13 && currDay >= 4)) {
+    currDay -= 2;
+  } else if (currWeek === 13 && currDay >= 3) {
+    // Offset to account for holiday (Wednesday only)
+    currDay -= 1;
+  }
+
+  return Math.max(Math.min(currDay, 33), 1).toString();
+}
+
+/**
+ * Calculates the current week number based on a start date, with a minimum of 1
+ * 
+ * @param startDate The date to use as a starting point (week 0)
+ * 
+ * @returns The current week number
+ * 
+ * @author Logan Gill
+ */
+function currentWeekNumber(startDate: Date): number {
+  // Today's date
+  const today = new Date();
+  // Reset time to midnight since we do not care about hours
+  today.setHours(0, 0, 0, 0);
+  // Get the difference between the two times (in milliseconds)
+  const msDiff = today.getTime() - startDate.getTime();
+  // Convert day days
+  const dayDiff = msDiff / (1000 * 60 * 60 * 24);
+  // Calculate the current week number
+  const currWeek = Math.max(Math.ceil(dayDiff / 7), 1);
+  return currWeek;
+}
+
 const config: Config = {
   title: 'NEU CS 2000 Public Resources',
   tagline: 'Resources for CS 2000 (Public)',
@@ -52,8 +182,6 @@ const config: Config = {
       {
         docs: {
           path: 'days',
-          routeBasePath: 'days',
-          sidebarPath: './sidebars/days.ts',
           editUrl:
             'https://github.com/neu-pdi/cs2000-public-resources/edit/main/',
           remarkPlugins: [remarkMath, createVariableSubstitutionPlugin(dcicDomain)],
@@ -98,6 +226,16 @@ const config: Config = {
         path: 'recitation',
         sidebarPath: './sidebars/recitation.ts',
         routeBasePath: 'recitation',
+        remarkPlugins: [createVariableSubstitutionPlugin(dcicDomain)],
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'days',
+        path: 'days',
+        sidebarPath: './sidebars/days.ts',
+        routeBasePath: 'days',
         remarkPlugins: [createVariableSubstitutionPlugin(dcicDomain)],
       },
     ],
@@ -157,8 +295,9 @@ const config: Config = {
           label: 'Notes',
           items: [
             {
-              type: 'docSidebar',
-              sidebarId: 'daysSidebar',
+              type: 'doc',
+              docId: currentDayNumber(),
+              docsPluginId: 'days',
               label: 'Days',
             },
             {
@@ -181,21 +320,21 @@ const config: Config = {
         },
         {
           type: 'doc',
-          docId: '1',
+          docId: currentAssignmentNumber(),
           position: 'left',
           label: 'Homework',
           docsPluginId: 'homework',
         },
         {
           type: 'doc',
-          docId: '1',
+          docId: currentLabNumber(),
           position: 'left',
           label: 'Labs',
           docsPluginId: 'lab',
         },
         {
           type: 'doc',
-          docId: '1',
+          docId: currentAssignmentNumber(),
           position: 'left',
           label: 'Recitations',
           docsPluginId: 'recitation',
